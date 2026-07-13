fc2f77249e937f52b4583fbd37bededf42704484
CHuks <techsirchuks@gmail.com>
2026-07-13 17:36:38 +0100

    I added create subnet bloc

--- main.tf
+++ main.tf
@@ -19,8 +19,16 @@ resource "azurerm_resource_group" "terraform-learning-rg" {
 
 #Create Virtual Network
 resource "azurerm_virtual_network" "g5-vnet" {
-  name                = "g5-fintech-vnet"  
+  name                = "g5-vnet"
   location            = azurerm_resource_group.terraform-learning-rg.location
   resource_group_name = azurerm_resource_group.terraform-learning-rg.name
-  address_space       = ["10/0.0.0/16"]
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Subnet Creation
+resource "azurerm_subnet" "g5-subnet" {
+  name                 = "g5-subnet"
+  resource_group_name  = azurerm_resource_group.terraform-learning-rg.name
+  virtual_network_name = azurerm_virtual_network.g5-vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
